dbaa3e8f6ec502ff0da96e644849066513d3f18b
jiemojiemo <460706836@qq.com>
2022-05-02 17:22:30 +0800

    add moorer reverb plugin

---
+++ moorer_reverb/PluginProcessor.h
@@ -0,0 +1,59 @@
+#pragma once
+
+#include "libaa/processor/aa_moorer_reverb_processor.h"
+#include <juce_audio_processors/juce_audio_processors.h>
+
+#include "../libaa_processor_parameters_manager.h"
+//==============================================================================
+class AudioPluginAudioProcessor
+    : public juce::AudioProcessor,
+      public juce::AudioProcessorParameter::Listener {
+public:
+  //==============================================================================
+  AudioPluginAudioProcessor();
+  ~AudioPluginAudioProcessor() override;
+
+  //==============================================================================
+  void prepareToPlay(double sampleRate, int samplesPerBlock) override;
+  void releaseResources() override;
+
+  bool isBusesLayoutSupported(const BusesLayout &layouts) const override;
+
+  void processBlock(juce::AudioBuffer<float> &, juce::MidiBuffer &) override;
+  using AudioProcessor::processBlock;
+
+  //==============================================================================
+  juce::AudioProcessorEditor *createEditor() override;
+  bool hasEditor() const override;
+
+  //==============================================================================
+  const juce::String getName() const override;
+
+  bool acceptsMidi() const override;
+  bool producesMidi() const override;
+  bool isMidiEffect() const override;
+  double getTailLengthSeconds() const override;
+
+  //==============================================================================
+  int getNumPrograms() override;
+  int getCurrentProgram() override;
+  void setCurrentProgram(int index) override;
+  const juce::String getProgramName(int index) override;
+  void changeProgramName(int index, const juce::String &newName) override;
+
+  //==============================================================================
+  void getStateInformation(juce::MemoryBlock &destData) override;
+  void setStateInformation(const void *data, int sizeInBytes) override;
+
+  void parameterValueChanged(int parameterIndex, float newValue) override;
+  void parameterGestureChanged(int parameterIndex,
+                               bool gestureIsStarting) override;
+
+private:
+  libaa::MoorerReverbProcessor proc;
+  libaa::AudioBlock block;
+
+  AudioEffectProcessorParametersManager parametersManager;
+  //==============================================================================
+  JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(AudioPluginAudioProcessor)
+};
